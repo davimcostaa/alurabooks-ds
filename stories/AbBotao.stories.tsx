841f7ac30130af40247cd5bd629a3e961c0a5cf7
Davi Costa <davim.costa@outlook.com>
2023-08-22 18:18:04 -0300

    Primeiro componente da biblioteca

---
+++ stories/AbBotao.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { AbBotao } from '../src'
+
+export default {
+    title: 'Components/AbBotao',
+    component: AbBotao
+} as ComponentMeta<typeof AbBotao>
+
+const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+
+export const Primario = Template.bind({})
