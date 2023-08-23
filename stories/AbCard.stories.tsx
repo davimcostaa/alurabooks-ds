487fdd02485d5907d7518dcb1f19adf0b92be00a
Davi Costa <davim.costa@outlook.com>
2023-08-23 18:29:36 -0300

    :rocket: Evoluindo o botão :rocket:

---
+++ stories/AbCard.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+import { AbCard } from '../src/components/AbCard'
+
+
+export default {
+    title: 'Components/AbBotao',
+    component: AbCard
+} as ComponentMeta<typeof AbCard>
+
+export const CardTemplate: ComponentStory<typeof AbCard> = () => <AbCard>
+    <h1>Card</h1>
+</AbCard>
