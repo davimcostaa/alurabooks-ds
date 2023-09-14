bb4d9dfdaf8f913c3e898cc16f623ffae28357e6
Davi Marques <davim.costa@outlook.com>
2023-09-14 11:58:41 -0300

    Atualizando componente de Tag

--- src/components/AbTag/index.tsx
+++ src/components/AbTag/index.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export interface AbTagProps {
+export interface AbTagStyleProps {
+  contexto?: 'primario' | 'secundario'
+}
+
+export interface AbTagProps extends AbTagStyleProps {
     texto: string
 }
 
 const Tag = styled.div`
-    padding: 48px;
-    background: #EB9B00;
+    padding: 16px 24px;
+    background: ${(props: AbTagStyleProps) => props.contexto === 'primario' ? '#EB9B00' : 'linear-gradient(97.54deg, #002F52 35.49%, #326589 165.37%)'}
     font-weight: 700;
     font-size: 24px;
     display: inline-block;
     font-family: sans-serif;
+    padding: 
 `
 
-const AbTag = ({texto}: AbTagProps) => {
-  return (
-    <Tag>{texto}</Tag>
-  )
-}
+export const AbTag = ({ texto, contexto = 'primario' }: AbTagProps) => {
+    return (
+        <Tag contexto={contexto}>
+            {texto}
+        </Tag>
+    )
 
-export default AbTag
+}
